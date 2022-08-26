f8da86d98e619e5e333cff02539ebd83c8cd986f
Xue Mei Lin <alu0101225845@ull.edu.es>
2022-08-27 00:36:37 +0100

    create server with mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,26 @@
-console.log('hello world')
+require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+
+const databaseURL = process.env.MONGODB_URL || 'mongodb://localhost/vilaflor';
+
+const mongoose = require('mongoose');
+mongoose.connect(databaseURL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database!'));
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+const port = process.env.PORT || 8081;
+
+app.listen(port, () => {
+  console.log(`app is up on port ${port}`)
+});
